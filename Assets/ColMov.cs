61fa14336875ff40b747d8290f4908b92ebdf2ec
Amafin <amandine.finas@efrei.net>
2026-08-28 19:41:57 +0200

    Réglage écart colonnes

--- Assets/ColMov.cs
+++ Assets/ColMov.cs
@@ -3,8 +3,26 @@ using UnityEngine;
 public class ColMov : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private float speed = 4f;
+    private float speed = 3f;
     private float leftLimit = -15f;
+
+    public Transform topPipe;
+    public Transform botPipe;
+
+    private float minGap = 5.5f;
+    private float maxGap = 8f;
+
+
+    void Start()
+    {
+        float gap = Random.Range(minGap, maxGap);
+
+        if (topPipe != null && botPipe != null)
+        {
+            topPipe.localPosition = new Vector3(0, gap / 2f, 0);
+            botPipe.localPosition = new Vector3(0, -gap / 2f, 0);
+        }
+    }
 
     private void Update()
     {
