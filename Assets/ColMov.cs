274451b3ad5f586eef2994b196b81f41d7cd28cf
Amafin <amandine.finas@efrei.net>
2026-08-28 20:03:47 +0200

    Écart aléatoire final + enlever disparition tuyau si trop haut/trop bas

--- Assets/ColMov.cs
+++ Assets/ColMov.cs
@@ -6,24 +6,6 @@ public class ColMov : MonoBehaviour
     private float speed = 3f;
     private float leftLimit = -15f;
 
-    public Transform topPipe;
-    public Transform botPipe;
-
-    private float minGap = 5.5f;
-    private float maxGap = 8f;
-
-
-    void Start()
-    {
-        float gap = Random.Range(minGap, maxGap);
-
-        if (topPipe != null && botPipe != null)
-        {
-            topPipe.localPosition = new Vector3(0, gap / 2f, 0);
-            botPipe.localPosition = new Vector3(0, -gap / 2f, 0);
-        }
-    }
-
     private void Update()
     {
         transform.position += (Vector3.left * speed) * Time.deltaTime;
--- Assets/PipeSpawner.cs
+++ Assets/PipeSpawner.cs
@@ -4,11 +4,24 @@ public class PipeSpawner : MonoBehaviour
 {
     public GameObject pipePrefab;
     private float spawnRate = 2f;
-    private float heightOffset = 2.4f;
+
+    private float minGap = 4.7f;
+    private float maxGap = 6f;
+
+    private float screenMargin = 1.5f;
+
 
     private float timer = 0f;
+
+    private float screenTopY;
+    private float screenBottomY;
+
+
     void Start()
     {
+        screenTopY = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, 0)).y;
+        screenBottomY = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)).y;
+
         SpawnPipe();
     }
 
@@ -25,11 +38,26 @@ public class PipeSpawner : MonoBehaviour
 
     void SpawnPipe()
     {
-        //Calcul de la position aléatoire
-        float lowestPoint = transform.position.y - heightOffset;
-        float highestPoint = transform.position.y + heightOffset;
-        float randomY = Random.Range(lowestPoint, highestPoint);
 
-        Instantiate(pipePrefab, new Vector3(transform.position.x, randomY, 0), transform.rotation);
+        float gap = Random.Range(minGap, maxGap);
+
+        float minHoleY = screenBottomY + screenMargin + (gap / 2f);
+        float maxHoleY = screenTopY - screenMargin - (gap / 2f);
+        float holeCenterY = Random.Range(minHoleY, maxHoleY);
+
+        GameObject newPipes = Instantiate(pipePrefab, new Vector3(transform.position.x, holeCenterY, 0), Quaternion.identity);
+
+        Transform top = newPipes.transform.Find("pipe-green_0_top");
+        Transform bot = newPipes.transform.Find("pipe-green_0_bot");
+
+        if (top != null)
+        {
+            top.localPosition = new Vector3(0, gap / 2f, 0);
+        }
+
+        if (bot != null)
+        {
+            bot.localPosition = new Vector3(0, -gap / 2f, 0);
+        }
     }
 }
